5ee38ba0632d82bc0d198e08359f3cc3e5b7d730
Jon Bierma <jon.bierma@gmail.com>
2017-09-13 21:44:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,23 +40,7 @@ function viewCart() {
         str += " and " + item + " at $" + cart[1][item] +"."
       }
     }
-  } else {
-    var str="In your cart, you have "
-    for (var i=0; i<cart.length-1;i++){
-      for (var item in cart[i]){
-        if (cart[i].hasOwnProperty(item)){
-          str+= item + " at $" + cart[i][item] + ", "
-        }
-      }
-    }
-    for (var item in cart[cart.length-1]){
-      if (cart[cartlength-1].hasOwnProperty(item)){
-        str+= item + " at $" + cart[cart.length-1][item] + "."
-      }
-    }
-    console.log(str)
-
-  }
+  } 
 
 }
 
