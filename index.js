32a34a69023e328618bf10a988a0bd5d86c699f5
Jon Bierma <jon.bierma@gmail.com>
2017-09-13 22:02:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,8 +77,10 @@ function removeFromCart(item) {
   var cart=getCart()
   for (var i=0; i<cart.length;i++){
     if (cart[i].hasOwnProperty(item)){
-          var newCart=[...cart.slice(0,i), ...cart.slice(i+1)]
-          return newCart    
+        var newCart1 = cart.slice(0,i)
+        var newCart2 = cart.slice(i+1)
+          var newCart=[...newCart1, ...newCart2]
+          return newCart
     } else {
       console.log("That item is not in your cart.");
       return cart
