d7ff8f7752cc2ad033029fd67c87b9432a40520c
Jon Bierma <jon.bierma@gmail.com>
2017-09-13 20:55:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,19 +10,40 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.random()*100
+ cart.push({[item]: price})
+ console.log(item + " has been added to your cart.")
+ return cart
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty")
+  } else {
+    var cartString = `In your cart, you have `
+    for (int i = 1; i< cart.length; i++){
+      var str=Object.keys(cart[i])
+      cartString += `, ${str[0]} at $${str[1]},`
+    }
+  }
 }
 
 function total() {
-  // write your code here
+  let sum=0
+  var keys = Object.keys(cart)
+  for (var i = 0; i < cart.length; i++){
+    
+    sum += parseInt(cart[i][keys[i]])
+  }
+  return sum
 }
 
 function removeFromCart(item) {
-  // write your code here
+  var keys = Object.keys(cart)
+  for (var i=0; i<cart.length;i++){
+    
+    
+  }
 }
 
 function placeOrder(cardNumber) {
